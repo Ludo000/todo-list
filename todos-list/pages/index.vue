2517c7058fc7cfece6af0e637b33c4fa3a8717b4
Scholz Ludovic <ludovic.scholz@gfi.fr>
2019-05-03 16:10:55 +0200

    feat: data are correctly retrieved from the backend, Vuetify is setting up and working

--- todos-list/pages/index.vue
+++ todos-list/pages/index.vue
@@ -1,28 +1,54 @@
 <template>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-    </li>
+<v-container grid-list-lg>
+  <v-layout row>
+      <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">TODOS</v-flex>
+  </v-layout>
+  <v-layout justify-center>
+    <v-container fluid grid-list-md>
+      <v-layout row wrap>
+        <v-card v-for="todo in todos" :key="todo.id">
+          <v-card-title :class="{ done: todo.done }">{{ todo.text }}</v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange" @change="toggle(todo)">Done</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-layout>
+    </v-container>
+  </v-layout>
+    
     <li><input placeholder="What needs to be done?" @keyup.enter="addTodo"></li>
-  </ul>
+
+</v-container>
 </template>
 
 <script>
 
 import { mapMutations, mapActions } from 'vuex'
 
+
 export default {
+  mounted : function() {
+            this.$store.dispatch('todos/getTodos')
+
+  },
   computed: {
+    ...mapActions({
+      getTodos: 'todos/getTodos'
+    }),
     todos () {
-      this.$store.dispatch('todos/getTodos')
+      
       return this.$store.state.todos.list
     }
   },
   methods: {
+    loadTodos(){
+        this.$store.dispatch('todos/getTodos')
 
+    },
     addTodo (e) {
       this.$store.commit('todos/add', e.target.value)
+      this.$store.dispatch('todos/sendTodos', e.target.value)
+
       e.target.value = ''
     },
     ...mapMutations({
--- todos-list/store/todos.js
+++ todos-list/store/todos.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios';
-
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
 Vue.use(Vuex)
 
 export const state = () => ({
@@ -13,61 +15,53 @@ export const mutations = {
         state.list.push({
             text: text,
             done: false
-    })
+          })
     },
     remove (state, { todo }) {
         state.list.splice(state.list.indexOf(todo), 1)
     },
     toggle (state, todo) {
         todo.done = !todo.done
+    },
+    setTodos(state, payload) {
+        payload.forEach(element => {
+            state.list.push({
+                text: element['title'],
+                done: false
+        })
+            
+        });
+    }
+}
+
+export const actions = {
+    async getTodos({ state, commit }) {
+        try {
+            let response = await axios.get('https://jsonplaceholder.typicode.com/todos').then((r) => {       
+                commit('setTodos', r.data);
+              })
+        } catch (error) {
+            commit('setTodos', []);
+        }
+    },
+    async sendTodos(text, { state, commit }){
+        try {
+            let response = await axios.post('https://jsonplaceholder.typicode.com/todos',{
+                    userId : 1,
+                    id : 999999,
+                    title : text,
+                    completed : false
+                }).then((r) => {       
+              })
+        } catch (error) {
+            commit('setTodos', []);
+        }
     }
 }
 
 
 new Vuex.Store({
-    state: () => ({
-        counter: 0,
-        apiUrl: 'https://jsonplaceholder.typicode.com/todos'
-    }),
-    mutations: {
-        increment (state) {
-            state.counter++
-        }
-    },
-    modules: {
-        todos: {
-            namespaced: true,
-            state: () => ({
-            list: []
-            }),
-            mutations: {
-                setTodos(state, payload) {
-                    state.list = payload;
-                },
-                add (state, { text }) {
-                    state.list.push({
-                    text,
-                    done: false
-                    })
-                },
-                remove (state, { todo }) {
-                    state.list.splice(state.list.indexOf(todo), 1)
-                },
-                toggle (state, { todo }) {
-                    todo.done = !todo.done
-                }
-            },
-            actions: {
-                async getTodos({ state, commit }) {
-                    try {
-                        let response = await axios.get(`${state.apiUrl}`)
-                        commit('setTodos', response.data.hits);
-                    } catch (error) {
-                        commit('setTodos', []);
-                    }
-                }
-            }
-        }
-    
-    }
+    state: state,
+    mutations: mutations,
+    actions: actions
 })
