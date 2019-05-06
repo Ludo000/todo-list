049987d84acb8c1e227b424c75c95240c52f42bf
Scholz Ludovic <ludovic.scholz@gfi.fr>
2019-05-06 11:27:51 +0200

    style: changing title string and adding some CSS

--- todos-list/pages/index.vue
+++ todos-list/pages/index.vue
@@ -1,24 +1,64 @@
 <template>
 <v-container grid-list-lg>
   <v-layout row>
-      <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">TODOS</v-flex>
+      <v-flex xs12 class="text-xs-center display-1 font-weight-black my-2">Liste à faire</v-flex>
   </v-layout>
-  <v-layout justify-center>
-    <v-container fluid grid-list-md>
-      <v-layout row wrap>
-        <v-card v-for="todo in todos" :key="todo.id">
-          <v-card-title :class="{ done: todo.done }">{{ todo.text }}</v-card-title>
-          <v-card-actions>
-            <v-btn flat color="orange" @change="toggle(todo)">Done</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-layout>
-    </v-container>
+    <input class="todos-input" placeholder="Entrez ici votre nouvelle chose à faire" @keyup.enter="addTodo"/>
+
+  <v-layout align-center justify-center row>
+
+      <v-flex xs6 class="bigcard">
+        <v-flex xs12 class="text-xs-center display-1 my-1">À faire</v-flex>
+        <v-container grid-list-md text-xs-center class="todos-container">
+          <v-layout wrap>
+          <div v-for="todo in todos" :key="todo.id" class="todo-container">
+            <v-flex v-if="!todo.done">
+              <v-card  color="#ffe260">
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn flat color="orange" @click="remove(todo)">✖</v-btn>
+                </v-card-actions>
+                <v-card-title :class="{ done: todo.done }">{{ todo.text }}</v-card-title>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn flat color="green" @click="toggle(todo)">✔</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+            </div>
+          </v-layout>
+        </v-container>
+      </v-flex>
+
+      <v-flex xs6 class="bigcard">
+                <v-flex xs12 class="text-xs-center display-1 my-1">Fait</v-flex>
+
+        <v-container grid-list-md text-xs-center class="todos-container">
+          <v-layout wrap>
+            <div v-for="todo in todos" :key="todo.id"  class="todo-container">
+            <v-flex v-if="todo.done">
+              <v-card  color="#ffe260">
+
+                <v-card-actions>
+                  <v-btn flat color="orange" @click="toggle(todo)">⮌</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn flat color="red" @click="remove(todo)" >✖</v-btn>
+                </v-card-actions>
+                <v-card-title :class="{ done: todo.done }">{{ todo.text }}</v-card-title>
+                
+              </v-card>
+            </v-flex>
+            </div>
+          </v-layout>
+        </v-container>
+      </v-flex>
+
   </v-layout>
-    
-    <li><input placeholder="What needs to be done?" @keyup.enter="addTodo"></li>
+
+
 
 </v-container>
+
 </template>
 
 <script>
@@ -58,4 +98,38 @@ export default {
   }
 }
 </script>
+
+<style>
+body{
+  background-color: #f8f7f7;
+}
+
+.bigcard
+ {
+   background-color :#e4e4e4;
+   margin : 20px;
+   border-radius : 20px;
+ }
+.todos-container
+{
+  background-color : white;
+  height : 60vh;
+  overflow: auto;
+  border-radius : 10px;
+}
+
+.todos-input
+{
+  width : 100%;
+  height : 5vh;
+  background-color : white;
+  padding : 20px;
+  border-radius : 50px;
+}
+
+.todo-container
+{
+    width : 33%;
+}
+</style>
 
